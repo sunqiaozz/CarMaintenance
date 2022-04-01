cfbeca3e18efaab213bcd967715e3540c9a7618a
zhangyu <zhangyu@2022.com>
2022-04-01 14:30:31 +0800

    V2.1

--- src/main/java/com/zy/maintenance/MaintenanceApplication.java
+++ src/main/java/com/zy/maintenance/MaintenanceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MaintenanceApplication {
 
     public static void main(String[] args) {
+        System.out.println("gitee test");
         System.out.println("github test2");
         System.out.println("zz merge test2");
         System.out.println("github test");
